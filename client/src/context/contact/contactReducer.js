f2fdfa92a55131b34fc3030d4698ab561f8a8d6b
betonit007 <timnagorski@hotmail.com>
2019-08-26 17:05:05 -0400

    Finished contact submission form and setting up contact context, chapter 54

---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,18 @@
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types';
+
+export default (state, action) => {
+    switch(action.type) {
+        case ADD_CONTACT:
+            return {...state, contacts: [...state.contacts, action.payload ]} //state is immutable so we have to take current state.contacts and add the payload
+        default:
+            return state;
+    }
+}
