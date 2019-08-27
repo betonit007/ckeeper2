2e33981e50c149112be6233b9fb929f15ab4211f
betonit007 <timnagorski@hotmail.com>
2019-08-27 12:41:47 -0400

    Finished updating contact, chapter 57

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -9,24 +9,33 @@ import {
 } from '../types';
 
 export default (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case ADD_CONTACT:
-            return {...state, contacts: [...state.contacts, action.payload ]} //state is immutable so we have to take current state.contacts and add the payload
+            return { ...state, contacts: [...state.contacts, action.payload] } //state is immutable so we have to take current state.contacts and add the payload
+
+        case UPDATE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact
+                )
+            }
+
         case DELETE_CONTACT:
-            return {...state, 
-            contacts: state.contacts.filter(contact => contact.id !== action.payload)
-        }
+            return {
+                ...state,
+                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+            }
         case SET_CURRENT:
             console.log('hello')
             return {
                 ...state, current: action.payload
             }
-            
+
         case CLEAR_CURRENT:
             return {
                 ...state, current: null
             }
-            default:
+        default:
             return state;
     }
 }
