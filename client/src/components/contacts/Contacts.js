b019c9f1c36ad68590a1103b5f16f025341c7db1
betonit007 <timnagorski@hotmail.com>
2019-08-28 10:39:16 -0400

    basic add, delete and CSS Transition Animations, chapter 59 finished

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -6,21 +6,32 @@ import ContactContext from '../../context/contact/contactContext';
 
 
 const Contacts = () => {
-    const contactContext = useContext(ContactContext);
-    const { contacts, filtered } = contactContext;
+  const contactContext = useContext(ContactContext);
+  const { contacts, filtered } = contactContext;
 
-    if(contacts.length === 0) {
-      return <h4>Please add a Contact!</h4>
-    }
+  if (contacts.length === 0) {
+    return <h4>Please add a Contact!</h4>
+  }
 
-    return (
-        <Fragment>
-           {filtered !== null   // if filtered is empty map over all contacts else map over filtered contacts
-           ? filtered.map(contact=> <ContactItem key={contact.id} contact={contact} />)
-           : contacts.map(contact => <ContactItem key={contact.id} contact={contact} />)
-           }
-        </Fragment>
-    )
+  return (
+
+    <Fragment>
+      <TransitionGroup>
+        {filtered !== null   // if filtered is empty map over all contacts else map over filtered contacts
+          ? filtered.map(contact => 
+            <CSSTransition key={contact.id} timeout={500} classNames="item">
+              <ContactItem  contact={contact} />
+            </CSSTransition>
+            )
+          : contacts.map(contact => 
+            <CSSTransition key={contact.id} timeout={500} classNames="item">
+              <ContactItem  contact={contact} />
+            </CSSTransition>
+            )
+        }
+      </TransitionGroup>
+    </Fragment>
+  )
 }
 
 export default Contacts;
