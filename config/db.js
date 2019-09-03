775e4b3f0b8e73c0a65a073002399385ed01cd1c
betonit007 <timnagorski@hotmail.com>
2019-09-03 15:13:48 -0400

    mongoURI config possible issues

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI'); // get connect info from gloabl variables stored in default.json file under config
+const db = config.get('mongodb+srv://betonit007:Nyjets$41@contactkeeper-1ukv3.mongodb.net/test?retryWrites=true&w=majority'); // get connect info from gloabl variables stored in default.json file under config
 
 const connectDB = async () => {
     mongoose.connect(db, {
