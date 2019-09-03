2afa0af0dc785e28788a09d8d0c2bf0d5b3f8c75
betonit007 <timnagorski@hotmail.com>
2019-09-03 14:14:21 -0400

    reset, trying to deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path') //deals with file paths
 
 const app = express();
 
@@ -16,6 +17,14 @@ app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Keeper API!'}));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve react in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (res, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 
 const PORT = process.env.PORT || 5000; //process.env.PORT is for production
