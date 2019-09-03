0d4892f9cb7beb0b62aab834e0d74f638237c7d7
betonit007 <timnagorski@hotmail.com>
2019-09-03 14:28:37 -0400

    server changes; trying to deploy

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ connectDB();
 app.use(express.json({ extended: false })); //now can accept data from req.body
 
 // Define Routes
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
@@ -21,7 +20,7 @@ if(process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (res, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 
